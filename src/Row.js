e7748dfd8f95df65e4547c0a10bf73650b5be5d2
Qi Cao <alanqicao@gmail.com>
2020-12-31 19:18:31 -0700

    change pic to large

--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import './Row.css';
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow}) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -27,8 +27,9 @@ function Row({ title, fetchUrl }) {
         {/*ser row poster */}
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${isLargeRow? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
